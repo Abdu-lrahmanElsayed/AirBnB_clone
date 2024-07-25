ae5bb5531b558a472e7bb5ac352bccecc82a0406
Abdu <Abdu>
2024-07-25 09:53:00 -0700

    task 9

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from models.place import Place
+import unittest
+
+
+class TestPlace(unittest.TestCase):
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_models/test_review.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from models.review import Review
+import unittest
+
+
+class TestReview(unittest.TestCase):
+
+
+if __name__ == '__main__':
+    unittest.main()
