2c12478b4add07f078641c43f7c8db8a0f2a191f
Abdu <Abdu>
2024-07-25 07:40:24 -0700

    task 5

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
 import unittest
-from models import storage
+import models
+import os
+from models.engine.file_storage import FileStorage
 
 
 class TestBaseModel(unittest.TestCase):
@@ -9,21 +11,26 @@ class TestBaseModel(unittest.TestCase):
     def test___objects(self):
         model = BaseModel()
         model.save()
-        self.assertTrue(storage.__objects)
+        self.assertTrue(models.storage.__objects)
 
     def test_all(self):
-        stor = storage.all()
-        self.assertTrue(stor.__objects)
+        self.assertEqual(type(models.storage.all()), dict)
 
     def test_new(self):
         model = BaseModel()
+        models.storage.new(model)
+        self.assertIn("BaseModel.{}".format(model.id), models.storage.all())
 
     def test_save(self):
         model = BaseModel()
-        self.assertTrue(model.save())
+        models.storage.new(model)
+        models.storage.save()
+        self.assertTrue(os.path.isfile("json.file"))
 
     def test_reload(self):
         model = BaseModel()
+        models.storage.reload()
+        self.assertIn("BaseModel.{}".format(model.id), models.storage.all())
 
 
 if __name__ == '__main__':
