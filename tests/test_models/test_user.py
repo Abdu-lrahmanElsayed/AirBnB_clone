b47b7d2bab6e6243f067f63c50da152eb1cf34ea
Abdu <Abdu>
2024-07-25 09:30:48 -0700

    task 8

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -6,24 +6,28 @@ import unittest
 class TestUser(unittest.TestCase):
 
     def test_email(self):
+        self.assertEqual(str, type(User.email))
         u = User()
         u.email = "airbnb@mail.com"
         u.save()
         self.assertEqual(u.email, "airbnb@mail.com")
 
     def test_password(self):
+        self.assertEqual(str, type(User.password))
         u = User()
         u.password = "root"
         u.save()
         self.assertEqual(u.password, "root")
 
     def test_first_name(self):
+        self.assertEqual(str, type(User.first_name))
         u = User()
         u.first_name = "Betty"
         u.save()
         self.assertEqual(u.first_name, "Betty")
 
     def test_last_name(self):
+        self.assertEqual(str, type(User.last_name))
         u = User()
         u.last_name = "Bee"
         u.save()
